cead2c3f98cbcc374e039d55e7c7b813a5345651
annemoraes <ilarymoraes@hotmail.com>
2021-01-14 22:28:58 -0300

    isolando componentes

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import LogoImg from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => (
@@ -12,9 +14,9 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu Login</h1>
 
-        <input placeholder="Email" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
