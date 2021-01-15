3ac041c2ddb7f8be63c3e6336c9cc5cf5f82d118
annemoraes <ilarymoraes@hotmail.com>
2021-01-14 22:57:17 -0300

    separando estilos dos componentes

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
 import Button from '../../components/Button';
@@ -14,8 +14,8 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu Login</h1>
 
-        <Input name="email" placeholder="Email" />
-        <Input name="password" type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
         <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
