0487e6caaaaa417db7b58a72e04959928166a7f6
annemoraes <ilarymoraes@hotmail.com>
2021-02-02 00:38:18 -0300

    adicionando unform

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Wave, BackgroundImg } from './styles';
 import Button from '../../components/Button';
@@ -7,25 +8,32 @@ import Input from '../../components/Input';
 
 import WaveImg from '../../assets/wave.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <form>
-        <h1>Faça seu Login</h1>
+const SignIn: React.FC = () => {
 
-        <Input name="email" icon={FiMail} placeholder="Email" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-        <Button type="submit">Entrar</Button>
+  function handleSubmit(data : object) : void {
+    console.log(data);
+  }
 
-        <a href="forgot">Esqueci minha senha</a>
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
-      </form>
-    </Content>
-    <BackgroundImg />
-  </Container>
-);
+  return(
+    <Container>
+      <Content>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Login</h1>
+
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+          <a href="login">
+            <FiLogIn />
+            Criar conta
+          </a>
+        </Form>
+      </Content>
+      <BackgroundImg />
+    </Container>
+  );
+};
 
 export default SignIn;
